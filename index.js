a4a154c9fcc38510dae0aa1c0758a409c385af39
TLCChen <139132998+TLCChen@users.noreply.github.com>
2023-10-10 13:18:09 -0400

    10-10-23

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const date = new Date();
-const year = date.getFullYear();
-const Bob = {
-    DOB: 2008,
-    name: "Bob",
-    age: return year - BOD
-}
+const animals = [
+  {
+    name: "Asian Tiger",
+    genus: "Panthera",
+    specific: "tigris tirgris",
+    location: ["India, Nepal, China"],
+    endangered: true,
+  },
+
+  {
+    name: "Dhole",
+    genus: "Cuon",
+    specific: "alpinus",
+    location: ["Central Asia", "South Asia", "East Asia", "Southeast Asia"],
+    endangered: true,
+  },
+
+  {
+    name: "Clouded Leopard",
+    genus: "Neofelis",
+    specific: "nebulosa",
+    location: ["Northeast India", "Bhutan", "South China"],
+    endangered: true,
+  },
+  {
+    name: "African Crested Porcupine",
+    genus: "Hystrix",
+    specific: "cristata",
+    location: ["Italy", "North Africa", "Sub-Saharan Africa"],
+    endangered: true,
+  },
+];
+
+animals.forEach((nam) => console.log(nam.name));
+animals.forEach((place) => place.forEach((land) => console.assert.log(land)));
